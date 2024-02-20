6dbdb95faaedd7f4bdffe60aac791b754e57fcb9
MerakiShubh <shubhamt.champ@gmail.com>
2024-02-20 23:24:09 +0530

    Navigation

--- frontend/src/Componets/shared/Navigation/Navigation.jsx
+++ frontend/src/Componets/shared/Navigation/Navigation.jsx
@@ -1,13 +1,25 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
+import styles from "./Navigation.module.css";
 
 const Navigation = () => {
+  const brandStyle = {
+    color: "#fff",
+    textDecoration: "none",
+    fontweight: "bold",
+    fontSize: "22px",
+    display: "flex",
+    alignitems: "center",
+  };
+
+  const logoText = {
+    marginLeft: "10px",
+  };
   return (
-    <nav>
-      <Link to="/">
+    <nav className={`${styles.navbar} container`}>
+      <Link to="/" style={brandStyle}>
         <img src="/images/logo.png" alt="logo" />
-        <span>Dropin</span>
+        <span style={logoText}>Dropin</span>
       </Link>
     </nav>
   );
