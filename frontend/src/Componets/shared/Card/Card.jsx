e8b14057ca6656847e428e228069740f2cce2d4d
MerakiShubh <shubhamt.champ@gmail.com>
2024-02-21 01:03:37 +0530

    Home modified

--- frontend/src/Componets/shared/Card/Card.jsx
+++ frontend/src/Componets/shared/Card/Card.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ title, icon, children }) => {
   return (
     <div className={styles.card}>
-      <div className={headingwrapper}>
-        <img src="/images/logo.png" alt="logo" />
-        <h1 className={styles.heading}>Welcome to Dropin</h1>
+      <div className={styles.headingwrapper}>
+        <img src={`/images/${icon}.png`} alt="logo" />
+        <h1 className={styles.heading}>{title}</h1>
       </div>
+      {children}
     </div>
   );
 };
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 import Card from "../../Componets/shared/Card/Card";
+import Button from "../../Componets/shared/Button/Button";
 const Home = () => {
+  const signInLinkStyle = {
+    color: "#0077ff",
+    fontWeight: "bold",
+    textDecoration: "none",
+    marginLeft: "10px",
+  };
   return (
     <div className={styles.cardWrapper}>
-      <Card></Card>
+      <Card title="Welcome to Dropin!" icon="logo">
+        <p className={styles.text}>
+          We're working hard to get Dropin ready for everyone! shile we wrap up
+          the finishing touches, we're adding people gradually to make sure
+          nothing breaks
+        </p>
+        <div>
+          <Button text="Get your username" />
+        </div>
+        <div className={styles.signinWrapper}>
+          <span className={styles.hasInvite}>Have an invite text?</span>
+          <Link style={signInLinkStyle} to="/login">
+            Sign In
+          </Link>
+        </div>
+      </Card>
     </div>
   );
 };
