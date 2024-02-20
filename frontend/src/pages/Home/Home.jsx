d8ca04d4767daef3d5fc6c65ad96a6fd231b5857
MerakiShubh <shubhamt.champ@gmail.com>
2024-02-20 23:59:32 +0530

    HOme component

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+import Card from "../../Componets/shared/Card/Card";
 const Home = () => {
   return (
-    <>
-      <div>This is home page</div>
-    </>
+    <div className={styles.cardWrapper}>
+      <Card></Card>
+    </div>
   );
 };
 
