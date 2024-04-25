a80a84de1eedbf87ad2f374b2796c9485749749a
MerakiShubh <shubhamt.champ@gmail.com>
2024-04-25 21:26:44 +0530

    fix warning

--- frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
+++ frontend/src/pages/Steps/StepPhoneEmail/Phone/Phone.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, Card } from "../../../../Componets";
 import TextInput from "../../../../Componets/shared/TextInput/TextInput";
 import styles from "../StepPhoneEmail.module.css";
-
+import PropTypes from "prop-types";
 const Phone = ({ onNext }) => {
   const [phoneNumber, setPhoneNumber] = useState("");
   return (
@@ -16,12 +16,15 @@ const Phone = ({ onNext }) => {
           <Button text="Next" onClick={onNext} />
         </div>
         <p className={styles.bottomParagraph}>
-          By entering your number, you're agreeing to out Term of Service and
-          Privacy Policy. Thanks!
+          By entering your number, you&aproc;re agreeing to out Term of Service
+          and Privacy Policy. Thanks!
         </p>
       </div>
     </Card>
   );
+};
+Phone.propTypes = {
+  onNext: PropTypes.func.isRequired,
 };
 
 export default Phone;
